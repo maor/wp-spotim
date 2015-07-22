d87e79f84537567e2897320b7c5cc2fba82f3e93
Maor Chasen <maorhaz@gmail.com>
2015-07-22 16:56:14 +0300

    added Grunt support, plus Node dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,253 @@
+/**
+ * WP-SpotIM Makefile
+ */
+'use strict';
+
+module.exports = function(grunt) {
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+	// Project configuration.
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON('package.json'),
+
+		checktextdomain: {
+			standard: {
+				options:{
+					text_domain: 'wp-spotim',
+					keywords: [
+						// WordPress keywords
+						'__:1,2d',
+						'_e:1,2d',
+						'_x:1,2c,3d',
+						'esc_html__:1,2d',
+						'esc_html_e:1,2d',
+						'esc_html_x:1,2c,3d',
+						'esc_attr__:1,2d',
+						'esc_attr_e:1,2d',
+						'esc_attr_x:1,2c,3d',
+						'_ex:1,2c,3d',
+						'_n:1,2,4d',
+						'_nx:1,2,4c,5d',
+						'_n_noop:1,2,3d',
+						'_nx_noop:1,2,3c,4d'
+					]
+				},
+				files: [ {
+					src: [
+						'**/*.php',
+						'!node_modules/**',
+						'!build/**',
+						'!tests/**',
+						'!vendor/**',
+						'!*~'
+					],
+					expand: true
+				} ]
+			}
+		},
+
+		pot: {
+			options:{
+				text_domain: 'wp-spotim',
+				dest: 'language/',
+				keywords: [
+					// WordPress keywords
+					'__:1',
+					'_e:1',
+					'_x:1,2c',
+					'esc_html__:1',
+					'esc_html_e:1',
+					'esc_html_x:1,2c',
+					'esc_attr__:1',
+					'esc_attr_e:1',
+					'esc_attr_x:1,2c',
+					'_ex:1,2c',
+					'_n:1,2',
+					'_nx:1,2,4c',
+					'_n_noop:1,2',
+					'_nx_noop:1,2,3c'
+				]
+			},
+			files:{
+				src: [
+					'**/*.php',
+					'!node_modules/**',
+					'!build/**',
+					'!tests/**',
+					'!vendor/**',
+					'!*~'
+				],
+				expand: true
+			}
+		},
+
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'assets/js/settings.js'
+			]
+		},
+
+		watch: {
+			js: {
+				files: [
+					'assets/js/settings.js'
+				],
+				tasks: [
+					'jshint'
+				],
+				options: {}
+			}
+		},
+
+		bumpup: {
+			options: {
+				updateProps: {
+					pkg: 'package.json'
+				}
+			},
+			file: 'package.json'
+		},
+
+		replace: {
+			plugin_main: {
+				src: [ 'spot.php' ],
+				overwrite: true,
+				replacements: [
+					{
+						from: /Version: \d{1,1}\.\d{1,2}\.\d{1,2}/g,
+						to: 'Version: <%= pkg.version %>'
+					}
+				]
+			},
+			
+			readme: {
+				src: [ 'readme.txt' ],
+				overwrite: true,
+				replacements: [
+					{
+						from: /Stable tag: \d{1,1}\.\d{1,2}\.\d{1,2}/g,
+						to: 'Stable tag: <%= pkg.version %>'
+					}
+				]
+			}
+		},
+
+		shell: {
+			git_add_all : {
+				command: [
+					'git add --all',
+					'git commit -m "Bump to <%= pkg.version %>"'
+				].join( '&&' )
+			}
+		},
+
+		release: {
+			options: {
+				bump: false,
+				npm: false,
+				commit: false,
+				//tagName: 'v<%= version %>',
+				commitMessage: 'released v<%= version %>',
+				tagMessage: 'Tagged as v<%= version %>'
+			}
+		},
+
+		wp_readme_to_markdown: {
+			github: {
+				options: {
+					travisUrlRepo: 'https://travis-ci.org/maor/wp-spotim',
+					gruntDependencyStatusUrl: 'https://david-dm.org/maor/wp-spotim'
+				},
+				files: {
+					'README.md': 'readme.txt'
+				}
+			}
+		},
+
+		copy: {
+			main: {
+				src: [
+					'**',
+					'!node_modules/**',
+					'!build/**',
+					'!wp-assets/**',
+					'!bin/**',
+					'!.git/**',
+					'!tests/**',
+					'!.travis.yml',
+					'!.jshintrc',
+					'!README.md',
+					'!phpunit.xml',
+					'!vendor/**',
+					'!Gruntfile.js',
+					'!package.json',
+					'!.gitignore',
+					'!.gitmodules',
+					'!*~'
+				],
+				expand: true,
+				dest: 'build/'
+			}
+		},
+		
+		clean: {
+			//Clean up build folder
+			main: [
+				'build'
+			]
+		},
+
+		wp_deploy: {
+			deploy:{
+				options: {
+					plugin_slug: '<%= pkg.slug %>',
+					svn_user: 'KingYes',
+					build_dir: 'build/'
+				}
+			}
+		},
+
+		phpunit: {
+			classes: {
+				dir: ''
+			},
+			options: {
+				bin: 'phpunit',
+				bootstrap: 'tests/bootstrap.php',
+				colors: true
+			}
+		}
+		
+	} );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [
+		'checktextdomain',
+		'jshint',
+		'pot',
+		'phpunit',
+		'wp_readme_to_markdown'
+	] );
+
+	grunt.registerTask( 'build', [
+		'default',
+		'clean',
+		'copy'
+	] );
+
+	grunt.registerTask( 'publish', [
+		'checktextdomain',
+		'pot',
+		'jshint',
+		'phpunit',
+		'bumpup',
+		'replace',
+		'wp_readme_to_markdown',
+		'shell:git_add_all',
+		'release'
+	] );
+};
