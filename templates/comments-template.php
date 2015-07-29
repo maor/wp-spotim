7ac5de5b22ffe921b7094bf29c7561a4f5b8ae0d
Maor Chasen <maorhaz@gmail.com>
2015-07-29 13:46:07 +0300

    add generic 10px padding to left and right of comment box

--- templates/comments-template.php
+++ templates/comments-template.php
@@ -1,3 +1,3 @@
 <div class="spot-im-comments comments-area">
-<div id="spot-im-frame-inpage"  data-post-id="<?php the_ID(); ?>"></div>
+<div id="spot-im-frame-inpage"  data-post-id="<?php the_ID(); ?>" style="padding: 0 10px;"></div>
 </div>
